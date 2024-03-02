feda8f935a4ff93bb484c4d584013e21abcfdc95
IhorRatsyn <ratsinjob@gmail.com>
2024-03-02 16:28:54 +0200

    styles added

--- src/Pages/MovieDetailsPage.jsx
+++ src/Pages/MovieDetailsPage.jsx
@@ -1,58 +1,66 @@
-import React, {useEffect, useState} from 'react';
-import {useParams, Outlet, Link} from "react-router-dom";
-import { getMovieDetail} from "../axios.js";
+import React, { useEffect, useState } from "react";
+import { useParams, Outlet, Link } from "react-router-dom";
+import { getMovieDetail } from "../axios.js";
 import BackButton from "../components/BackButton.jsx";
+import styles from "./MovieDetailsPage.module.css";
 
-const MovieDetailsPage = props => {
-  const [movie, setMovie] = useState(null)
-  const {movieId} = useParams();
+const MovieDetailsPage = (props) => {
+  const [movie, setMovie] = useState(null);
+  const { movieId } = useParams();
 
   useEffect(() => {
-    getMovieDetail(movieId).then((result)=> {
-      setMovie(result)
-    })
-  }, [])
-
+    getMovieDetail(movieId).then((result) => {
+      setMovie(result);
+    });
+  }, []);
 
   const getImgUrl = (url) => {
-    return `https://image.tmdb.org/t/p/w500/${url}`
-  }
-  if (!movie) return
-
+    return `https://image.tmdb.org/t/p/w500/${url}`;
+  };
+  if (!movie) return;
 
   return (
-    <div>
-      <BackButton/>
-      <img src={getImgUrl(movie.poster_path)} alt={movie.title}/>
-      <h1>{movie.title}</h1>
-      <p>User score {movie.vote_average}%</p>
-      <h3>Overview</h3>
-      <p>{movie.overview}</p>
-      <h3>Genres</h3>
-      <div>
-        {movie.genres.map((genre) => (
-          <div key={genre.id}>
-              {genre.name}
-          </div>
-        ))}
+    <div className={styles.container}>
+      <BackButton />
+      <div className={styles.posterContainer}>
+        <img
+          className={styles.posterImg}
+          src={getImgUrl(movie.poster_path)}
+          alt={movie.title}
+        />
       </div>
-      <hr/>
-      <h3>Adiitional information</h3>
-      <ul>
-        <li><Link to={`/movies/${movie.id}/reviews`}>
-          Reviews
-        </Link></li>
-        <li> <Link to={`/movies/${movie.id}/cast`}>
-          Cast
-        </Link></li>
-      </ul>
-      <Outlet />
+      <div className={styles.content}>
+        <div className={styles.topContent}>
+          <h1>{movie.title}</h1>
+          <p>User score {movie.vote_average}%</p>
+          <h3>Overview</h3>
+          <p>{movie.overview}</p>
+          <h3>Genres</h3>
+          <div>
+            {movie.genres.map((genre) => (
+              <div key={genre.id}>{genre.name}</div>
+            ))}
+          </div>
+          <hr />
+        </div>
+        <div className={styles.infoList}>
+          <h3>Adiitional information</h3>
+          <ul>
+            <li>
+              <Link to={`/movies/${movie.id}/reviews`}>Reviews</Link>
+            </li>
+            <li>
+              {" "}
+              <Link to={`/movies/${movie.id}/cast`}>Cast</Link>
+            </li>
+          </ul>
+        </div>
+        <Outlet />
+      </div>
     </div>
   );
 };
 
-MovieDetailsPage.propTypes = {
-  
-};
+MovieDetailsPage.propTypes = {};
 
 export default MovieDetailsPage;
