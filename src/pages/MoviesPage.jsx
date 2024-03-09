e80ffbae6843b1198227dd51f071252c037efe6b
IhorRatsyn <ratsinjob@gmail.com>
2024-03-09 19:28:57 +0200

    add useSearchParams

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react";
 import SearchBar from "../SearchBar.jsx";
 import MovieList from "./MovieList.jsx";
 import { getMovies, getMoviesSearch } from "../axios.js";
+import { useSearchParams } from "react-router-dom";
 
 const MoviesPage = (props) => {
   const [movies, setMovies] = useState([]);
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useSearchParams("");
   useEffect(() => {
     getMoviesSearch(query).then((result) => {
       setMovies(result.results);
